5c00f4fb63448c090d53810b96ae68d3d22d420a
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-11-02 20:38:30 -0300

    while en python

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from funciones.utils import calcularArea
+import sys
 
-def main():
-    """Esta es la funcion main
+def main() -> int:
+    """Este la funcion principal main
+
+    Returns:
+        int: si devuelve 0 el programa termino bien..
     """
-    print("Hello World")
+    isRunning = True
     calcularArea(10,35)
+
+    contador = 0
+
+    while isRunning:
+        print("Hello World")
+
+        if contador >= 10:
+            isRunning = False
+
+        print(contador)
+        contador += 1
+
+    return 0
+        
 
 # Esta linea dice "si estoy ejecutando python como una app arranco por aca"
 if __name__ == '__main__':
-    main()
-
+    sys.exit(main())
