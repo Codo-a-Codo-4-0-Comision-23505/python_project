0a89f07f0c4f2ce19f042c78031efb2c4823bab4
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-11-06 20:43:16 -0300

    python code

--- app.py
+++ app.py
@@ -12,6 +12,28 @@ def main() -> int:
 
     contador = 0
 
+    frase = "HOla mundo en el 6 de noviembre" #Esto es un array de caracteres...
+
+    print(frase) # Esto imprime la frase completa
+    print(frase[0]) # Esto imprime la letra H
+    print(frase[0:4]) # Estamos sacando solo el HOla
+    print(frase[5:10]) #Mostramos la segunda la palabra
+    print(frase[10:])
+    print(frase[:10])
+    print(frase[:5] + "World" + frase[10:])
+    print(frase[::2]) # 
+    print(frase[:5:-1]) #Imprime al reves
+    print(frase.replace("mundo", "world"))
+    
+    print(list(frase))
+
+    frase2 = ['h', 'o', 'l', 'a']
+    print(frase2)
+    print(str(frase2))
+
+    diccionario = {'Juan': 56, 'Ana': 15}
+    print(diccionario)
+    
     while isRunning:
         print("Hello World")
 
